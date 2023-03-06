f03a45f25028fb0a9a6ca98dd1e81cc30d791906
Celorio2001 <jahir.celorio2001@gmail.com>
2023-03-05 21:39:35 -0500

    Añadir la Plantilla de Veterinaria

---
+++ resources/views/Plantilla/header.blade.php
@@ -0,0 +1,46 @@
+<header class="header_section">
+    <div class="container-fluid">
+      <nav class="navbar navbar-expand-lg custom_nav-container ">
+        <a class="navbar-brand" href="index.html">
+          <img src="images/logo.png" alt="">
+          <span>
+            Amigos Peludos
+          </span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <div class="d-flex mx-auto flex-column flex-lg-row align-items-center">
+            <ul class="navbar-nav  ">
+              <li class="nav-item">
+                <a class="nav-link" href="service">Servicios</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="pet.html">Galería</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="clinic.html">Registro</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.html">Contacto</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="buy.html">Tienda</a>
+              </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0 ml-0 ml-lg-4 mb-3 mb-lg-0">
+              <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit"></button>
+            </form>
+          </div>
+          <div class="quote_btn-container  d-flex justify-content-center">
+            <a href="">
+              Call: +01 1234567890
+            </a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
